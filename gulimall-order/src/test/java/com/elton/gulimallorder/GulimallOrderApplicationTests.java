b4c884a22e42640704cfa9c0b16aea955b31f333
elton <gexin7648@hotmail.com>
2021-06-20 18:25:28 +1000

    gulimall init

---
+++ gulimall-order/src/test/java/com/elton/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elton.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
